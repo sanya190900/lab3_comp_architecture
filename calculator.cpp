87c551cd5f1c38e265a4995d022daae4ce8e0ee6
Alexandr <sanya190900@gmail.com>
2019-10-20 06:10:36 -0400

    Changing some strings

--- calculator.cpp
+++ calculator.cpp
@@ -18,3 +18,15 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+// some modification
+// adding division operation
+// for integer and double types
+// int Calculator::Div(int a, int b){
+//	return a : b
+//	}
+//int Calculator::Div(double a, double b){
+//	return a : b + 0.5
+//	}
+// we have 4 basic operations
+// this file was modified too
+// the end
--- calculator.h
+++ calculator.h
@@ -10,7 +10,17 @@ class Calculator
 =======
         int Add (int, int);
         int Mul (double, double);
+
+// my changes
+// Adding division operation 
+// for integer and double types 
+// int Div(int, int);
+// int Div(double, double);
+// we have 4 basic operations
+// and this file was modified	
+// I complete task
 >>>>>>> e0ea21b... add a multiplication operation
 };
+
 
 #endif//CALCULATOR_H
